f19a4a3aa13f1ae403fdf81b650d23f0f4ca816a
kiran <ugalekiran29@gmail.com>
2023-09-03 23:16:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Topbar from './Navbar/Topbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Topbar></Topbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Topbar(){
+
+    return(
+     <>
+     <div className='navbar '>
+       top  Navbar
+     </div>
+     </>
+    );
+}
